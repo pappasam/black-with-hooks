b4982bf3704c0bd4809808339916c67804fc3462
Sam Roeca <samuel.roeca@gmail.com>
2018-10-25 15:20:39 -0400

    Added main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
-'''The main module'''
+"""The main module"""
 
+import os
+import shutil
+from os.path import (
+    _joinrealpath,
+    abspath,
+    basename,
+    commonpath,
+    commonprefix,
+    devnull,
+    getatime,
+)
+
+
+def very_long_function_name_please_stop(
+    akjsdkfjaskdf, lajsdlkfjklasjdfk, alisdjflkajsdf, lakjskldf
+):
+    """A great section"""
